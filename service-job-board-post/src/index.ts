0fe7ef4b9f8798fb0097407288f0242ed9ca42ab
Ajay <ajaygangarde@gmail.com>
2025-01-12 12:24:35 +0530

    implementation of SNS AND SQS

--- service-job-board-post/src/index.ts
+++ service-job-board-post/src/index.ts
@@ -1,10 +1,38 @@
+
 import express, { Request, Response } from 'express';
+// import entire SDK
+import AWS from 'aws-sdk';
+const  SNS = new AWS.SNS({region:"us-east-1"})
 
 const app = express();
 const port = 4001;
 
+const publishJobPostedEvent = async (jobDetails:any) => {
+    const params = {
+      TopicArn: 'arn:aws:sns:us-east-1:194722438131:NewJobPostedTopic', // Replace with your Topic ARN
+      Message: JSON.stringify(jobDetails),
+    };
+  
+    try {
+      await SNS.publish(params).promise();
+      console.log('Job posted event published to SNS');
+    } catch (error) {
+      console.error('Error publishing job event:', error);
+    }
+  };
+
 app.get('/', (req: Request, res: Response) => {
-    res.send('Job Board Post Service');
+
+    // Example usage
+publishJobPostedEvent({
+    jobId: '12345',
+    title: 'Senior Fullstack Developer',
+    location: 'Remote',
+    company: 'Tech Solutions',
+    description: 'Join our team as a Senior Fullstack Developer...',
+  });
+
+res.send('Job Board Post Service');
 });
 
 app.listen(port, '0.0.0.0', () => {
